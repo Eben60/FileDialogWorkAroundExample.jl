024d1275ad0b3994b8960f90a84cafa331aa74e0
Ben Elkin <8xe8n-cm@online.de>
2024-12-15 15:22:50 +0100

    pick_multifile

--- src/NatFlDlgWorkAround.jl
+++ src/NatFlDlgWorkAround.jl
@@ -2,7 +2,7 @@ module NatFlDlgWorkAround
 
 using NativeFileDialog, Dates
 
-"Returns OS version on Mac or v0 if other OS"
+"Returns OS version on Mac, or v0 if other OS"
 function macos_version()
     Sys.isapple() || return v"0"
     osversion = read(`sw_vers --productVersion`, String)
@@ -17,6 +17,13 @@ function pick_file(path=""; filterlist="")
     return pick_workaround(path, :pickfile; filterlist)
 end
 export pick_file
+
+
+function pick_multi_file(path=""; filterlist="") 
+    BUGGY_MACOS || return NativeFileDialog.pick_file(path; filterlist)
+    return pick_workaround(path, :multifile; filterlist)
+end
+export pick_multi_file
 
 function pick_folder(path="") 
     BUGGY_MACOS || return NativeFileDialog.pick_folder(path)
@@ -39,9 +46,14 @@ function check_if_log_noise(s, starttime)
     return nothing
 end
 
+
+# with multiple selections allowed
+
 function pick_workaround(path, picktype; filterlist="")
     if picktype == :pickfile
         script_trunk = """POSIX path of (choose file with prompt "Pick a file:" """
+    elseif picktype == :multifile
+        script_trunk = """(choose file with prompt "Pick a file:" with multiple selections allowed """
     elseif picktype == :pickfolder
         script_trunk = """POSIX path of (choose folder with prompt "Pick a folder:" """
     else
@@ -83,8 +95,25 @@ function pick_workaround(path, picktype; filterlist="")
         warn_noise = ""
     end
     check_if_log_noise(warn_noise, starttime)
-    return flpath
+    picktype == :multifile || return flpath
+    return parse_multifiles(flpath)
 end
 
+function parse_multifiles(p)
+    # p = "alias MacSysdiskBen:Users:elk:Downloads:2023:tmp:Abrechnung_,31.pdf, alias MacSysdiskBen:Users:elk:Downloads:2023:tmp:Abrechnung_2,574767229.pdf, alias MacSysdiskBen:Users:elk:Downloads:2023:tmp:Abrechnung_214712029800,73.pdf"
+    p1 = replace(p, ", alias " => "\nalias ")
+    als = split(p1, "\n")
+    for (i, al) in pairs(als)
+        startswith(al, "alias ") || error("cannot parse $al")
+        als[i] = al[7:end]
+    end
+    return alias2posix.(als)
+end
+
+function alias2posix(fl)
+    script = """POSIX path of "$fl" """ 
+    cmd = `osascript -e $(script)`
+    return readchomp(cmd)
+end
 
 end # module NatFlDlgWorkAround
--- src/NatFlDlgWorkAroundTest.jl
+++ src/NatFlDlgWorkAroundTest.jl
@@ -4,8 +4,9 @@ include("NatFlDlgWorkAround.jl")
 using .NatFlDlgWorkAround
 # export macos_version
 # export check_if_log_noise
+# export parse_multifiles, alias2posix
 
-export pick_file, pick_folder
+export pick_file, pick_folder, pick_multi_file
 
 
 end # module NatFlDlgWorkAroundTest
